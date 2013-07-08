0f87e3ff2309b2e0ce0c18de9ae8533fb3c9b64d
NanoSurfer <shawnbrewis@gmail.com>
2013-07-08 02:58:01 +0200

    Flipping lost GUI (Arrrgghh)

--- src/budgettool/BudgetMain.java
+++ src/budgettool/BudgetMain.java
@@ -12,15 +12,15 @@ public class BudgetMain {
     
     public static void main(String args[])
     {
-        BudgetGUI myGui = new BudgetGUI();
-        myGui.setVisible(true);
-//        MonthlyBudget myBudget = new MonthlyBudget();
-//        
-//        double myIncome = myBudget.setIncome(20000);
-//        myBudget.setWorkDays(20);
-//        
-//        System.out.println("The monthly income with TAX is : " + myBudget.calculateTax(myIncome));
-//        System.out.println("The daily income per day with TAX : " + myBudget.incomePerDay());
+//        BudgetGUI myGui = new BudgetGUI();
+//        myGui.setVisible(true);
+        MonthlyBudget myBudget = new MonthlyBudget();
+        
+        double myIncome = myBudget.setIncome(20000);
+        myBudget.setWorkDays(20);
+        
+        System.out.println("The monthly income with TAX is : " + myBudget.calculateTax(myIncome));
+        System.out.println("The daily income per day with TAX : " + myBudget.incomePerDay());
     }
     
 }
